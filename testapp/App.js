1dd86f5e18b0f1279478f67ce82ca59652f638ea
Carlos Jimenez <carlos_jmnz@hotmail.com>
2021-09-02 15:05:35 -0500

    layout

--- testapp/App.js
+++ testapp/App.js
@@ -1,41 +1,73 @@
-import React, { useReducer } from 'react'
-import { View, Text, TextInput } from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet } from 'react-native'
 
+import Toggle from './Toggle'
 
-function reducer(state, action) {
-    switch (action.type) {
-      case 'first':
-        return {...state, first: action.value}
-    
-      case 'last':
-        return {...state, last:action.value}
-    }
+export default function App() {
+  const [flexDirection, setFlexDirection] = useState('row')
+  const [justifyContent, setJustifyContent] = useState('center')
+  const [alignItems, setAlignItems] = useState('center')
+  const layoutStyle = { flexDirection, justifyContent, alignItems }
+
+  const primaryAxis = flexDirection === 'row' ? 'Horizontal' : 'Vertical'
+  const secondaryAxis = flexDirection === 'row' ? 'Vertical' : 'Horizontal'
+
+  return (
+    <View style={styles.container}>
+      <Toggle
+        label={'Primary axis (flexDirection)'}
+        value={flexDirection}
+        options={flexDirectionOptions}
+        onChange={(option) => {
+          setFlexDirection(option)
+        }}
+      />
+      <Toggle
+        label={`${primaryAxis} distribution (justifyContent)`}
+        value={justifyContent}
+        options={justifyContentOptions}
+        onChange={(option) => {
+          setJustifyContent(option)
+        }}
+      />
+      <Toggle
+        label={`${secondaryAxis} alignment (alignItems)`}
+        value={alignItems}
+        options={alignItemsOptions}
+        onChange={(option) => {
+          setAlignItems(option)
+        }}
+      />
+      <View style={[styles.layout, layoutStyle]}>
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+      </View>
+    </View>
+  )
 }
 
-export default function App(){
-  const [state, dispatch]= useReducer(reducer, {first:'', last:''} )
+const flexDirectionOptions = ['row', 'column']
+const justifyContentOptions = [
+  'flex-start',
+  'center',
+  'flex-end',
+  'space-around',
+  'space-between',
+]
+const alignItemsOptions = ['flex-start', 'center', 'flex-end', 'stretch']
 
-return(
-  <View>
-    <TextInput
-      style={{fontSize: 32}}
-      placeholder="First"
-      value={state.first}
-      onChangeText={(text)=>{
-        dispatch({type:'first', value: text})
-      }}
-    />
-    <TextInput
-      style={{fontSize:32}}
-      placeholder="Last"
-      value={state.last}
-      onChangeText={(text)=>{
-        dispatch({type:'last',value:text})
-      }}/>
-    <Text style={{fontSize:32}}>
-      hello {state.first} {state.last}
-    </Text>
-  </View>
-)
-
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  layout: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.05)',
+  },
+  box: {
+    padding: 25,
+    backgroundColor: '#3B6CD4',
+    margin: 5,
+  },
+})
